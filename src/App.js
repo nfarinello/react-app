84f9f8bacf38c624f4111ade05fb9b10cc840425
Nicole Farinello <nicole_mattos@live.com>
2024-05-02 23:23:10 +0100

    adding the details of the open sourced website and link to my personal page and github repo link

--- src/App.js
+++ src/App.js
@@ -5,6 +5,13 @@ export default function App() {
   return (
    <div classname="App">
     Weather App
+
+    <footer>
+      <p>This is a website built by <a href="https://jolly-wing-8b84f9.netlify.app" target="_blank"
+      alt="Nicoles personal website/portfolio" rel="noreferrer">Nicole Farinello</a> and is
+      <a href="https://github.com/nfarinello/react-app" target="_blank" rel="noreferrer"> Open-Sourced</a>
+      </p>
+    </footer>
     </div>
   );
 }
