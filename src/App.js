f3e14c92c225423b2588698b075173d19f90537a
Nicole Farinello <nicole_mattos@live.com>
2024-05-03 15:11:11 +0100

    adding weather component and creating the js file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import './App.css';
+import Weather from "./Weather";
 
 export default function App() {
   return (
    <div className="App">
     <div className="container">
     Weather App
-
+    <Weather />
     <footer>
       <p>This is a website built by <a href="https://jolly-wing-8b84f9.netlify.app" target="_blank"
       alt="Nicoles personal website/portfolio" rel="noreferrer">Nicole Farinello</a> and is
