7ef9a3db2de21f6bce1ac3de5a34847955589a2d
Nicole Farinello <nicole_mattos@live.com>
2024-05-14 00:06:13 +0100

    added space

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ export default function App() {
 
     <footer>
       <p>This is a website built by <a href="https://jolly-wing-8b84f9.netlify.app" target="_blank"
-      alt="Nicoles personal website/portfolio" rel="noreferrer">Nicole Farinello</a> and is
+      alt="Nicoles personal website/portfolio" rel="noreferrer">Nicole Farinello </a> and is
       <a href="https://github.com/nfarinello/react-app" target="_blank" rel="noreferrer"> Open-Sourced</a>
       </p>
     </footer>
