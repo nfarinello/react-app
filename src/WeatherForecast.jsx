8207d1324aa63e716474292d3e3ab10a269f2148
Nicole Farinello <nicole_mattos@live.com>
2024-05-16 16:46:54 +0100

    updated the temperature for all the days of the week to match the city on the search

--- src/WeatherForecast.jsx
+++ src/WeatherForecast.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./WeatherForecast.css";
 import axios from "axios";
 import WeatherForecastDay from "./WeatherForecastDay.jsx";
@@ -6,6 +6,10 @@ import WeatherForecastDay from "./WeatherForecastDay.jsx";
 export default function WeatherForecast(props) {
   let [loaded, setLoaded] = useState(false);
   let [forecast, setForecast] = useState(null);
+
+useEffect (() => {
+setLoaded(false);
+}, [props.coordinates]);
 
 function handleResponse(response) {
   setForecast(response.data.daily);
@@ -15,9 +19,16 @@ if (loaded) {
   return (
     <div className="WeatherForecast">
         <div className="row">
-          <div className="col">
-            <WeatherForecastDay data={forecast[0]} />
+          {forecast.map(function (dailyForecast, index) {
+            if (index < 5){
+            return (
+          <div className="col" key={index}>
+            <WeatherForecastDay data={dailyForecast} />
           </div>
+        );
+      }
+      return null; // Add this line
+      })}
         </div>
       </div>
       );
--- src/WeatherForecastDay.jsx
+++ src/WeatherForecastDay.jsx
@@ -26,9 +26,11 @@ export default function WeatherForecastDay(props) {
         <div className="WeatherForecast-day">{day()}</div>
           <WeatherIcon code={props.data.weather[0].icon} size={36} />
             <div className="WeatherForecast-Temperatures">
-              <span className="WeatherForecast-temperature-max">{maxTemperature()}
+              <span className="WeatherForecast-temperature-max">
+                {maxTemperature()}
               </span>
-              <span className="WeatherForecast-temperature-min">{minTemperature()}
+              <span className="WeatherForecast-temperature-min">
+                {minTemperature()}
               </span>
             </div>
         </div>
